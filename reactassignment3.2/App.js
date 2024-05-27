89be0c7dcac9c63981e4f41a66bf9854c7da6618
Ahmad2Anzar <161143818+Ahmad2Anzar@users.noreply.github.com>
2024-05-27 14:13:43 +0530

    reactassignment3.2

---
+++ reactassignment3.2/App.js
@@ -0,0 +1,13 @@
+import ChangeColor2 from "./background";
+import FocusInput2 from "./focus";
+
+function App() {
+  return (
+    <div className="App">
+    <ChangeColor2/>
+    <FocusInput2/>
+    </div>
+  );
+}
+
+export default App;
